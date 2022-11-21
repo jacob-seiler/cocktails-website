a5a0112c9e0f2b86d8d9ba77a124943bcd792431
Jacob Seiler <seiler.jacob1@gmail.com>
2022-11-21 15:25:19 -0500

    Made search a client component

--- app/Search.tsx
+++ app/Search.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Search() {
+    const [text, setText] = useState("")
+    
     return (
-        <input type="text" className="border" placeholder="Search" />
+        <>
+            <input type="text" className="border" placeholder="Search" value={text} onChange={e => setText(e.target.value)} />
+            <p>{text}</p>
+        </>
     )
 }
